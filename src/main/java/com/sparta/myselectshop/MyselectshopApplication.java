70784772eacbaea03e18f7bb0e96c0f9a5dba51e
Gyutae Bae <qoxowkd0716@gmail.com>
2023-12-01 15:30:09 +0900

    Error 메시지

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -2,7 +2,6 @@ package com.sparta.myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -12,5 +11,4 @@ public class MyselectshopApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyselectshopApplication.class, args);
     }
-
 }
